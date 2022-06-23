70af3df27ed1555136e056f1405fba668a0e9655
chrisdaaz <chris-diaz@northwestern.edu>
2022-06-23 15:37:32 -0500

    fix syntax errors

--- bin/deploy_aws.sh
+++ bin/deploy_aws.sh
@@ -3,21 +3,25 @@
 BUILD_DIR="site"
 BUCKET_ID="city-public-value-and-capitalism.northwestern.pub"
 DIST_ID="E3MDI919TWPVU3"
+PDF_FILE="city-public-value-and-capitalism"
 
 echo "Deleting old publication"
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 
+echo "Generating PDF"
+quire pdf --file=$PDF_FILE
+
 echo "Generating site"
 quire site
 
 echo "Uploading website files to AWS s3"
-aws s3 sync 
+aws s3 sync \
     $BUILD_DIR \
-    $BUCKET_ID \
+    s3://$BUCKET_ID \
     --delete
 
 echo "Clearing AWS CloudFront Cache"
 aws cloudfront create-invalidation \
     --distribution-id $DIST_ID \
-    --paths "/"
+    --paths "/*"
